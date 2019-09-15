362c10fd650efbf309c3003c03af87a4a962e864
qbhan <smarthkb98@gmail.com>
2019-09-15 22:40:00 +0900

    added server

---
+++ server/server/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  server
+//
+//  Created by 한규범 on 15/09/2019.
+//  Copyright © 2019 Q. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
